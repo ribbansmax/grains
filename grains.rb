d00a7046692bfe8a022fb2f5a0e8de8108fcea7d
Max Ribbans <ribbans.max@gmail.com>
2021-04-26 11:39:58 -0400

    Add string logic

--- grains.rb
+++ grains.rb
@@ -1,3 +1,15 @@
-def num_grains()
-
+def num_grains(square = nil)
+  if square.nil?
+    string = '1'
+    63.times do
+      string << '1'
+    end
+    string.to_i(2)
+  else
+    string = '1'
+    (square - 1).times do
+      string << '0'
+    end
+    string.to_i(2)
+  end
 end
